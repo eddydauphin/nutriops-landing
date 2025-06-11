a405a8781ef5dedeea63266281682a7dbc934516
eddydauphin <eddyraymond.dauphin@gmail.com>
2025-06-11 12:55:30 +0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import NutriOpsLandingPage from "./NutriOpsLandingPage.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <NutriOpsLandingPage />
+  </React.StrictMode>
+);
